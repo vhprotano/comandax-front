d394c5dd7305dd8a01a065d64055d3efb0ec8459
vhprotano <vhprotano@hotmail.com>
2025-11-11 19:29:21 +0000

    Update environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   // Configure your Google OAuth Client ID here
   // Get it from: https://console.cloud.google.com/apis/credentials
   googleClientId: '453451322163-dig5i8jftell1mfvu1t97evcqov7uetn.apps.googleusercontent.com',  
-  apiUrl: 'http://localhost:5012/graphql',
+  apiUrl: 'https://comandax-api.onrender.com/graphql',
 };
 
